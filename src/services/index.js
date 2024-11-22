b3ba729712792302d67299959fff6103faad9541
Jasmitsingh01 <97171634+Jasmitsingh01@users.noreply.github.com>
2024-11-23 00:10:10 +0530

    url error fixed

--- src/services/index.js
+++ src/services/index.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 export const FormDataApi = axios.create({
   withCredentials: true,
-    baseURL: "http://localhost:4000/api/",
-    headers: {
+  baseURL: "https://dnk-backend.onrender.com/api/",
+  headers: {
     "Access-Control-Allow-Origin": "*",
     "Content-Type": "application/multipart/form-data",
     Authorization: "Bearer " + localStorage.getItem("_user_access_token"),
@@ -12,7 +12,7 @@ export const FormDataApi = axios.create({
 
 export const api = axios.create({
   withCredentials: true,
-  baseURL: "http://localhost:4000/api/",
+  baseURL: "https://dnk-backend.onrender.com/api/",
   headers: {
     "Access-Control-Allow-Origin": "*",
     Authorization: "Bearer " + localStorage.getItem("_user_access_token"),
